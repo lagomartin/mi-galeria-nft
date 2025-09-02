40b500549da3f2b741b9a22660a0876f0e5669ae
lagomartin <martinlago84@gmail.com>
2025-09-02 08:54:07 -0300

    Corregir configuración de ThirdwebProvider con clientId

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ThirdwebProvider } from "thirdweb/react";
+// IMPORTANTE: Usamos la versión del SDK compatible con el resto del proyecto
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+
+// Tu Client ID de Thirdweb
+const clientId = "81dbf33bbc52674567a6c73dc257bf20";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThirdwebProvider>
+    <ThirdwebProvider
+      activeChain="sepolia"
+      clientId={clientId}
+    >
       <App />
     </ThirdwebProvider>
   </React.StrictMode>,
